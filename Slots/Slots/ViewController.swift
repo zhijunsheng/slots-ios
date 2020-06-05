43d3985c8e8701c5ffcae3ca4ae67c18c733861f
Zhijun Sheng <zhijunsheng@MacBook-Pro.local>
2020-06-05 07:53:49 -0400

    created new xcode project Slots

---
+++ Slots/Slots/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slots
+//
+//  Created by Zhijun Sheng on 2020-06-05.
+//  Copyright © 2020 Zhijun Sheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
